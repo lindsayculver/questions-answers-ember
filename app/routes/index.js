3dd202eee25b046b8aed93625a28baadb1bdc773
Guest User <student@epicodus.com>
2015-09-25 10:46:52 -0700

    add new questions to the index page based on user input

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,6 +4,12 @@ export default Ember.Route.extend({
   },
 
   actions: {
+    save3(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.TransitionTo('index');
+    },
+    
     destroyQuestion(question) {
       question.destroyRecord();
       this.transitionTo('index');
