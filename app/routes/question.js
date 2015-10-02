0015b485b96cc38da4d33cc5a7fdd0ff38359828
Lindsay Culver <leculver@gmail.com>
2015-10-02 15:02:09 -0700

    refactor again for details page

--- app/routes/question.js
+++ app/routes/question.js
@@ -4,4 +4,17 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
   },
+
+  actions: {
+    save(params) {
+      var newAnswer =
+      this.store.createRecord('answer', params);
+      newAnswer.save();
+      this.transitionTo('question-detail', params.question);
+    
+    // destroyAnswer(answer) {
+    //   answer.destroyRecord();
+    //   this.transitionTo('question-detail');
+    }
+  }
 });
